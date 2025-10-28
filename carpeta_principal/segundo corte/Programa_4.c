39e781311a8ae30a687549057e39713983b9e74f
Valerie321 <vajhernandezp@udistrital.edu.co>
2025-10-28 17:44:29 -0500

    Programa 4.c

---
+++ carpeta_principal/segundo corte/Programa_4.c
@@ -0,0 +1,36 @@
+//Programa_4.c
+//VALERIE JIRETH HERNANDEZ PARDO 21-10-2025
+//AlMACENAMIENTO DE DATOS EN VARIABLES
+
+
+#include <stdio.h>
+int main()
+{
+int entero;
+float flotante;
+printf("Ingresa el valor entero: ");
+scanf("%i", &entero);
+printf("El valor ingresado es: %d\0", entero);
+printf("Ingresa el valor float: ");
+scanf("%f", &flotante);
+printf("El valor ingresado es: %f\0", flotante);
+return 0;
+}
+
+//Cuando esta el caracter n genera salto de linea. 
+//Cuando se coloca la a emita un sonido .
+//La letra r hace que el cursor vuelva al inicio de la línea.
+//La letra b  reduce una posición hacia atras.
+//Caudno colocamos la letra f cambia de página.
+//La letra t inserta un espacio en blanco que mueve el cursor a la siguiente posic ión de tabulación.
+//La letra v  nos desplaza el texto verticalmente.
+// El número  0 indica el final del codigo.
+
+
+
+
+
+
+
+
+
